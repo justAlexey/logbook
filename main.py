b7fb4a7fdd6ff13d05d6b6062b8dfa4971629e23
3dduck <test@test.ru>
2021-09-15 16:34:17 +0300

    add function for get string logbook

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from openpyxl import load_workbook
+import openpyxl as opxl
+
+def getInfo(data):
+	global ToW, UserSign,EndDate,UsedMHR,CMProject
+	for var in range(0,data.max_column):
+		if data[1][var].value == "ToW":
+			ToW=var
+		if data[1][var].value == "User Sign":
+			UserSign = var
+		if data[1][var].value == "End Date":
+			EndDate = var
+		if data[1][var].value == "Used MHR":
+			UsedMHR = var
+		if data[1][var].value == "CM Project":
+			CMProject = var
+
+
 
 try:
-	wb = load_workbook('./test.xlsx')
+	wb = opxl.open('test3.xlsx')
 except:
 	print('no file')
 	exit()
-print(wb.get_sheet_names)
+sheet = wb.active
+getInfo(sheet)
+
+for var in range(1,sheet.max_row+1):
+	logbook = "date:{date}|ACCMS|project:{project}|".format(date = sheet[var][EndDate].value , project = sheet[var][CMProject].value)
+	print(logbook)
+
