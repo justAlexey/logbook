d66d02b996662d0e3431e1a6d172a7601ac04911
justAlexey <90326187+justAlexey@users.noreply.github.com>
2021-10-15 17:13:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import openpyxl as opxl
-import prograss
+from progress.bar import IncrementalBar
 
 import logbook
 
 lb = logbook.Logbook()
 # print("clear 1225")
 lb.ws1225 = lb.clear(lb.ws1225)
+progress = IncrementalBar("create logbook...", max = lb.ws1225.max_row-1)
 for i in range(2,lb.ws1225.max_row):
     data = lb.formateRow(i)
     if data == None:
+        progress.next()
         continue
     lb.writeRow(lb.ws1225[i][lb.sbt].value, data)
+    progress.next
 lb.save()
+progress.finish()
 
