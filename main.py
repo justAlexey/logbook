bc73dded1c25d1b66d75a7423d46895922e493e4
root <root@ovz3.j1154134.meo8n.vps.myjino.ru>
2021-10-15 12:16:33 +0000

    edit main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import logbook
 lb = logbook.Logbook()
 # print("clear 1225")
 lb.ws1225 = lb.clear(lb.ws1225)
-progress = IncrementalBar("create logbook...", max = lb.ws1225.max_row-1)
+#print("cleared")
+progress = IncrementalBar("create logbook...", max = lb.ws1225.max_row)
 for i in range(2,lb.ws1225.max_row):
     data = lb.formateRow(i)
     if data == None:
