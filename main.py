127d44bbbd4c638732e544d231ad3c8e78937eb8
ovb.st44 <a.petrenko@s7.ru>
2021-10-16 19:29:14 +0700

    change 1225 1328 and addstaff list

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ lb = logbook.Logbook()
 # print("clear 1225")
 lb.ws1225 = lb.clear(lb.ws1225)
 #print("cleared")
-progress = IncrementalBar("create logbook...", max = lb.ws1225.max_row)
+#progress = IncrementalBar("create logbook...", max = lb.ws1225.max_row)
 for i in range(2,lb.ws1225.max_row):
     data = lb.formateRow(i)
     if data == None:
-        progress.next()
+#        progress.next()
         continue
     lb.writeRow(lb.ws1225[i][lb.sbt].value, data)
-    progress.next
+#    progress.next
 lb.save()
-progress.finish()
+#progress.finish()
 
