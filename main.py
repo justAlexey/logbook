b2b2bc2c5de7dfe96e56129958214658f54f93a4
Alexey <test@test.ru>
2021-10-18 02:14:51 +0700

    add check type of work in 1225 by database

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import openpyxl as opxl
 from progress.bar import IncrementalBar
 
 import logbook
+import database
 
 print("start")
 s = time.monotonic()
 lb = logbook.Logbook()
 # print("clear 1225")
 dif = time.monotonic()
-lb.ws1225 = lb.clear(lb.ws1225)
+lb.ws1225 = lb.clear1225(lb.ws1225)
+lb.ws1328 = lb.clear1328(lb.ws1328)
 
 print("cleared")
 progress = IncrementalBar("create logbook...", max = lb.ws1225.max_row-2)
 
 for i in range(2,lb.ws1225.max_row):
+    wsrow = lb.ws1225[i]
+    if wsrow[lb.tow].value not in database.typeofwork:
+        progress.next()
+        continue
     data = lb.formateRow(i)
     if data == None:
         progress.next()
